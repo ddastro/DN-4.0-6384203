d966b883ac2dd4b14e981ff6f762219fcefd34d4
ddastro <159152966+ddastro@users.noreply.github.com>
2025-06-25 20:10:42 +0530

    Update MyService.java

--- learning-program-solutions/WEEK 2/JUnit,Mockito and SL4J/3. Mockito exercises/Exercise 2: Verifying Interactions/MyService.java
+++ learning-program-solutions/WEEK 2/JUnit,Mockito and SL4J/3. Mockito exercises/Exercise 2: Verifying Interactions/MyService.java
@@ -1,1 +1,14 @@
+package com.example.mockito;
+
+public class MyService {
+    private ExternalApi externalApi;
+
+    public MyService(ExternalApi externalApi) {
+        this.externalApi = externalApi;
+    }
+
+    public void fetchData() {
+        externalApi.getData();  // Side effect or important call
+    }
+}
 
